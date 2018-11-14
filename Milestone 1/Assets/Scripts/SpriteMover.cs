bbf09cb86135b3659aa5cce5f770e3a703149add
swoofz <jordanhall.1995@yahoo.com>
2018-11-13 19:21:42 -0800

    Added SpriteMover Script to move sprite in the direction it is facing

---
+++ Milestone 1/Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour {
+
+    // Delcare variables
+    public float speed = 0.02f; // Variable to adjust the speed of the sprite
+    private Transform spriteTf; // Variable to get transform values
+
+	// Use this for initialization
+	void Start () {
+        spriteTf = GetComponent<Transform>(); // get transform values of component
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move sprite up
+        spriteTf.position += Vector3.up * speed; // Update y positive every frame
+	}
+}
